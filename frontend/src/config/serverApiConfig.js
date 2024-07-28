8e2d489ac56a519232ca63b5e94dd18dcbc45c9f
Tejas <tj970639@dal.ca>
2024-07-27 21:27:50 -0300

    modification to environment variables

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,23 +1,6 @@
-export const API_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
-    ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://3.213.136.34:8888/api/';
-export const BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://3.213.136.34:8888/';
-
-export const WEBSITE_URL = import.meta.env.PROD
-  ? 'http://cloud.idurarapp.com/'
-  : 'http://3.213.136.34:3000/';
-export const DOWNLOAD_BASE_URL =
-  import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
-    ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://3.213.136.34:8888/download/';
+export const API_BASE_URL = process.env.VITE_BACKEND_SERVER + 'api/';
+export const BASE_URL = process.env.VITE_BACKEND_SERVER;
+export const WEBSITE_URL = 'http://cloud.idurarapp.com/';
+export const DOWNLOAD_BASE_URL = process.env.VITE_BACKEND_SERVER + 'download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
-
-export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
-
-//  console.log(
-//    '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
-//  );
+export const FILE_BASE_URL = process.env.VITE_FILE_BASE_URL;
